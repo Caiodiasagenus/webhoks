c4d1faed52daaf431cc8c5047985b4f5b5a44e01
caio <caio.dias@agenus.com.br>
2026-01-25 13:54:52 -0300

    Refactor teste.ts by removing unnecessary console log statements and variable declarations

--- teste.ts
+++ teste.ts
@@ -15,30 +15,3 @@ console.log("teste 7")
 console.log("teste 8")
 console.log("teste 9")
 console.log("teste 10")
-
-const d = 4;
-console.log(d);
-console.log("teste 11")
-console.log("teste 12")
-console.log("teste 13")
-console.log("teste 14")
-console.log("teste 15")
-console.log("teste 16")
-console.log("teste 17")
-console.log("teste 18")
-console.log("teste 19")
-console.log("teste 20")
-
-
-const e = 5;
-console.log(e);
-console.log("teste 21")
-console.log("teste 22")
-console.log("teste 23")
-console.log("teste 24")
-console.log("teste 25")
-console.log("teste 26")
-console.log("teste 27")
-console.log("teste 28")
-console.log("teste 29")
-console.log("teste 30")
