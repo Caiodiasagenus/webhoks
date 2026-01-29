0589c44040cf9de803b219b8c436af298d6bf38a
caio <caio.dias@agenus.com.br>
2026-01-29 12:27:13 -0300

    Add additional console log statements for variables f, g, h, and i in teste.ts

--- teste.ts
+++ teste.ts
@@ -44,3 +44,16 @@ console.log(d);
 
 const e = 5;
 console.log(e);
+
+
+const f = 6;
+console.log(f);
+
+const g = 7;
+console.log(g);
+
+const h = 8;
+console.log(h);
+
+const i = 9;
+console.log(i);
