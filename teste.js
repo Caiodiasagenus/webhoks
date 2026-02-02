be17957643f90151a68daf7d6c74c9f9a725ac82
caio <caio.dias@agenus.com.br>
2026-02-02 20:05:53 -0300

    Add multiple console log statements for variables a, b, c, and d in teste.js and teste.ts; update source map accordingly

--- teste.js
+++ teste.js
@@ -1,4 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("teste");
+const a = 1;
+console.log(a);
+const b = 2;
+console.log(b);
+const c = 3;
+console.log(c);
+const d = 4;
+console.log(d);
 //# sourceMappingURL=teste.js.map
--- teste.ts
+++ teste.ts
@@ -1,1 +1,13 @@
 console.log("teste");
+
+const a = 1;
+console.log(a);
+
+const b = 2;
+console.log(b);
+
+const c = 3;
+console.log(c);
+
+const d = 4;
+console.log(d);
