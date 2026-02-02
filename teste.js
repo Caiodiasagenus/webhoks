d17e11fa20e7deff4fb45950b5607ccca6ef7db9
caio <caio.dias@agenus.com.br>
2026-02-02 20:09:42 -0300

    Add additional console log statements for variables e, f, g, h, and i in teste.ts

--- teste.js
+++ teste.js
@@ -9,4 +9,13 @@ const c = 3;
 console.log(c);
 const d = 4;
 console.log(d);
+const e = 5;
+console.log(e);
+const f = 6;
+console.log(f);
+const g = 7;
+console.log(g);
+const h = 8;
+console.log(h);
+const i = 9;
 //# sourceMappingURL=teste.js.map
--- teste.ts
+++ teste.ts
@@ -11,3 +11,17 @@ console.log(c);
 
 const d = 4;
 console.log(d);
+
+const e = 5;
+console.log(e);
+
+const f = 6;
+console.log(f);
+
+const g = 7;
+console.log(g);
+
+const h = 8;
+console.log(h);
+
+const i = 9;
