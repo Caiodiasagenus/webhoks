03eca3556cc13db2561036ce548b26d9f3e65380
caio <caio.dias@agenus.com.br>
2026-02-02 20:24:11 -0300

    Remove all console log statements from teste.js and teste.ts; update source map accordingly

--- teste.js
+++ teste.js
@@ -1,10 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("teste");
-const a = 1;
-console.log(a);
-const b = 2;
-console.log(b);
-const c = 3;
-console.log(c);
 //# sourceMappingURL=teste.js.map
