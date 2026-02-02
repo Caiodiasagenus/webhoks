9530dad1872fb3af23c798b7dbf069efab94ecc6
caio <caio.dias@agenus.com.br>
2026-02-02 18:08:36 -0300

    Add console log statements for new variables eja, ejb, ejc, and ejd in teste.js and teste.ts

--- teste.js
+++ teste.js
@@ -245,4 +245,12 @@ const eiv = 112;
 console.log(eiv);
 const eiw = 113;
 console.log(eiw);
+const eja = 114;
+console.log(eja);
+const ejb = 115;
+console.log(ejb);
+const ejc = 116;
+console.log(ejc);
+const ejd = 117;
+console.log(ejd);
 //# sourceMappingURL=teste.js.map
