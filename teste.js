dc1b4f26566a7e56ea53abb9864cdf6a61cad563
caio <caio.dias@agenus.com.br>
2026-02-03 15:35:50 -0300

    Refactor teste.js and teste.ts by removing unnecessary console log statements; update source map accordingly

--- teste.js
+++ teste.js
@@ -2,13 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("teste");
 const a = 1;
-console.log(a);
-const b = 2;
-console.log(b);
-const c = a + b;
-console.log(c);
-const d = a * b;
-console.log(d);
-console.log(c);
-console.log(d);
 //# sourceMappingURL=teste.js.map
