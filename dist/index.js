69643c73c02cd828723dfbf34fad70a806b143e6
caio <caio.dias@agenus.com.br>
2026-02-03 16:24:48 -0300

    Remove console log statements and unused variables from index.ts and update corresponding source maps

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello World");
-const a = 1;
-const b = 2;
-const c = a + b;
-console.log(c);
-console.log(c);
-console.log(c);
-console.log(c);
 //# sourceMappingURL=index.js.map
