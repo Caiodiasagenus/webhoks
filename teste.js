338c10116c1f9fe24e5cb8c006a8a8a06250ea53
caio <caio.dias@agenus.com.br>
2026-02-03 10:51:15 -0300

    Add console log statements for new variables b, c, and d in teste.js and teste.ts; update source map accordingly

--- teste.js
+++ teste.js
@@ -3,4 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 console.log("teste");
 const a = 1;
 console.log(a);
+const b = 2;
+console.log(b);
+const c = a + b;
+console.log(c);
+const d = a * b;
+console.log(d);
 //# sourceMappingURL=teste.js.map
